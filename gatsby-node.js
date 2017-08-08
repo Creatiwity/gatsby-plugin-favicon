e84e56204373ef2d7ed9a93d215e072d7c157c87
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2017-08-08 17:52:54 +0200

    Configure favicons to a fixed folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,4 +2,9 @@ const webpack = require(`webpack`);
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 exports.modifyWebpackConfig = ({ config }) =>
-  config.plugin(`Favicon`, FaviconsWebpackPlugin, ['./src/favicon.png'])
+  config.plugin(`Favicon`, FaviconsWebpackPlugin, [
+    {
+      logo: './src/favicon.png',
+      prefix: 'favicons/'
+    }
+  ])
