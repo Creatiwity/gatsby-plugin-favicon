0445ceb924ce4ff36d39f0f4a0cefa0197e7190b
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2018-06-17 22:55:38 -0400

    Gatsby v2 - onCreateWebpackConfig/setWebpackConfig

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,26 +1,27 @@
-import webpack from 'webpack'
 import FaviconsWebpackPlugin from 'favicons-webpack-plugin'
 
-exports.modifyWebpackConfig = ({ config, stage }, { logo, icons }) => {
+exports.onCreateWebpackConfig = ({ actions, stage }, { logo, icons = {} }) => {
   if (stage === 'develop-html' || stage === 'build-html') {
-    config.plugin(`Favicon`, FaviconsWebpackPlugin, [
-      {
-        logo: logo || './src/favicon.png',
-        prefix: 'favicons/',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: true,
-          favicons: true,
-          firefox: true,
-          twitter: true,
-          yandex: true,
-          windows: true,
-          ...icons,
-        },
-      },
-    ])
+    actions.setWebpackConfig({
+      plugins: [
+        new FaviconsWebpackPlugin({
+          logo: logo || './src/favicon.png',
+          prefix: 'favicons/',
+          icons: {
+            android: true,
+            appleIcon: true,
+            appleStartup: true,
+            coast: true,
+            favicons: true,
+            firefox: true,
+            twitter: true,
+            yandex: true,
+            windows: true,
+            ...icons,
+          }
+        }),
+      ]
+    })
   }
 }
 
