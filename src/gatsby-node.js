e71f0a5664ee1c0938f8f540418d37d8c4cb5795
Julien Blatecky <julien.blatecky@creatiwity.net>
2018-07-04 16:36:30 +0200

    Add background and title to the configuration

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,12 +1,14 @@
 import webpack from 'webpack'
 import FaviconsWebpackPlugin from 'favicons-webpack-plugin'
 
-exports.modifyWebpackConfig = ({ config, stage }, { logo, icons }) => {
+exports.modifyWebpackConfig = ({ config, stage }, { logo, icons, title, background }) => {
   if (stage === 'develop-html' || stage === 'build-html') {
     config.plugin(`Favicon`, FaviconsWebpackPlugin, [
       {
         logo: logo || './src/favicon.png',
         prefix: 'favicons/',
+        title,
+        background: background || '#fff',
         icons: {
           android: true,
           appleIcon: true,
