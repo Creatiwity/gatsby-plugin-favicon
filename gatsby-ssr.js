93a25560e0e8069a542d2e42d04edd962f2574c6
Julien Blatecky <julien.blatecky@creatiwity.net>
2018-07-24 00:12:32 +0200

    Fixes public path

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,11 @@
 const ReactParser = require('html-react-parser');
+const path = require('path');
 
 module.exports.onRenderBody = ({ setHeadComponents }) => {
-    const stats = require('./public/.iconstats.json');
+  const stats = require(path.join(process.cwd(), 'public', '.iconstats.json'));
   const prefix = __PATH_PREFIX__ ? __PATH_PREFIX__ : '';
   const prefixedOutputFilePrefix = `${prefix}/${stats.outputFilePrefix}`;
+
   setHeadComponents(stats.html.map(htmlRow =>
     ReactParser(htmlRow.replace(`/${stats.outputFilePrefix}`, prefixedOutputFilePrefix))
   ));
