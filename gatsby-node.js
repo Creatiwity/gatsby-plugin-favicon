be22b0d905a65cc4f4fbab6b3319f34cd462a1ed
Julien Blatecky <julien.blatecky@creatiwity.net>
2018-07-24 21:36:11 +0200

    Remove prefix, automagically handled by favicon plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,6 @@ module.exports.onCreateWebpackConfig = ({ actions, stage, getConfig }, { logo, i
           inject: false,
           emitStats: true,
           statsFilename: '.iconstats.json',
-          prefix: getConfig().output.publicPath + 'icons-[hash]/',
           icons: {
             android: true,
             appleIcon: true,
