8646b9b8e3f05d6b910a05115a6c8402fdfbaee1
Julien Blatecky <julien.blatecky@creatiwity.net>
2018-07-24 23:46:30 +0200

    Add manifest configuration options

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,49 @@
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
-module.exports.onCreateWebpackConfig = ({ actions, stage, getConfig }, { logo, icons = {}, title, background }) => {
+module.exports.onCreateWebpackConfig = ({
+  actions,
+  stage,
+  getConfig
+}, {
+  logo,
+  icons = {},
+  title,
+  appName,
+  appDescription,
+  developerName,
+  developerURL,
+  dir,
+  lang,
+  background,
+  theme_color,
+  display,
+  orientation,
+  start_url,
+  version
+}) => {
   if (stage === 'develop-html' || stage === 'build-html') {
     actions.setWebpackConfig({
       plugins: [
         new FaviconsWebpackPlugin({
           logo: logo || './src/favicon.png',
-          title,
-          background: background || '#fff',
-          inject: false,
           emitStats: true,
           statsFilename: '.iconstats.json',
+          persistentCache: true,
+          inject: false,
+
+          appName: appName || title,
+          appDescription: appDescription,
+          developerName: developerName,
+          developerURL: developerURL,
+          dir: dir || 'auto',
+          lang: lang || 'en-US',
+          background: background || '#fff',
+          theme_color: theme_color || '#fff',
+          display: display || 'standalone',
+          orientation: orientation || 'any',
+          start_url: start_url || '/?homescreen=1',
+          version: version || '1.0',
+
           icons: {
             android: true,
             appleIcon: true,
