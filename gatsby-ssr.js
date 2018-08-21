77283f35840afad07145178217eab5c11faae9df
Pyrax <piranhadev@gmail.com>
2018-08-21 20:38:22 +0200

    Add React key property to dom nodes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+const React = require('react')
 const ReactParser = require('html-react-parser');
 const path = require('path');
 const fs = require('fs');
@@ -6,5 +7,18 @@ module.exports.onRenderBody = ({ setHeadComponents }) => {
   const statsPath = path.join(process.cwd(), 'public', '.iconstats.json');
   const stats = JSON.parse(fs.readFileSync(statsPath, {encoding: 'utf8'}));
 
-  setHeadComponents(stats.html.map(htmlRow => ReactParser(htmlRow)));
+  let count = 1;
+
+  const parserOptions = {
+    // Insert key for React on the tags (if not already present)
+    replace: ({ attribs, name }) => {
+      if (attribs && !attribs.hasOwnProperty('key')) {
+        attribs.key = `gatsby-plugin-favicon-${count}`;
+        count++;
+        return React.createElement(name, attribs);
+      }
+    },
+  };
+
+  setHeadComponents(stats.html.map(htmlRow => ReactParser(htmlRow, parserOptions)));
 };
